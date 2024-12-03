4c61e801889bb6c547f674d23d0a9944cf4da01b
Star <creacciones@gmail.com>
2024-12-03 00:39:40 -0300

    * mejora de consumo API

---
+++ src/main/java/com/aluracursos/screenmatch/model/Serie.java
@@ -0,0 +1,27 @@
+package com.aluracursos.screenmatch.model;
+
+
+public class Serie {
+
+    private String titulo;
+// Asocia la propiedad JSON "Title" con el campo `titulo`.
+
+    private Integer totalTemporadas;
+// Asocia la propiedad JSON "totalSeasons" con el campo `totalTemporadas`.
+
+    private String evaluacion;
+// Asocia la propiedad JSON "imdbRating" con el campo `evaluacion`.
+
+    private String poster;
+// Asocia la propiedad JSON "Poster" con el campo `poster`.
+
+    private Categoria genero;
+// Asocia la propiedad JSON "Genre" con el campo `genero`.
+
+    private String actores;
+// Asocia la propiedad JSON "Actors" con el campo `actores`.
+
+    private String sinopsis;
+// Asocia la propiedad JSON "Plot" con el campo `sinopsis`.
+
+}

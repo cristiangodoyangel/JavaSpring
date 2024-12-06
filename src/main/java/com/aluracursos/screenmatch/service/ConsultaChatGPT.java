8fb07f3d55ec24876eefd22524bb6bbda22bf9b8
cristiangodoyangel <creacciones@gmail.com>
2024-12-05 21:09:12 -0300

    - agrega función de traducción de la API ChatGPT

--- src/main/java/com/aluracursos/screenmatch/service/ConsultaChatGPT.java
+++ src/main/java/com/aluracursos/screenmatch/service/ConsultaChatGPT.java
@@ -5,7 +5,7 @@ import com.theokanning.openai.service.OpenAiService;
 
 public class ConsultaChatGPT {
     public static String obtenerTraduccion(String texto) {
-        OpenAiService service = new OpenAiService("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");
+        OpenAiService service = new OpenAiService("TU API");
 
         CompletionRequest requisicion = CompletionRequest.builder()
                 .model("gpt-3.5-turbo")

3941adc3c0274a41b98b8952991987e6ad04263a
Star <creacciones@gmail.com>
2024-12-05 21:34:37 -0300

    * conexión con base de datos postgreSQL a través de pgAdmin

--- src/main/java/com/aluracursos/screenmatch/model/Serie.java
+++ src/main/java/com/aluracursos/screenmatch/model/Serie.java
@@ -36,7 +36,7 @@ public Serie(DatosSerie datosSerie) {
     this.poster = datosSerie.poster();
     this.genero = Categoria.fromString(datosSerie.genero().split(":")[0].trim());
     this.actores = datosSerie.actores();
-    this.sinopsis = ConsultaChatGPT.obtenerTraduccion(datosSerie.sinopsis()) ;
+    this.sinopsis = datosSerie.sinopsis();
                                     }
 
     @Override
